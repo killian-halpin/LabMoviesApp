bd54b5e6cf7a9a7c52d3a21913dbbf60810f14c3
Killian Halpin <75046987+killian-halpin@users.noreply.github.com>
2022-11-02 20:31:19 +0000

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {createRoot} from "react-dom/client";
+import HomePage from "./pages/homePage";
+import sample from './stpries/sampleData'
+//import MovieDetailsPage from './pages/movieDetailsPage'
+
+const movies = [sample, sample, sample, sample, sample, sample, sample];
+
+const App = () => {
+  return (
+      <HomePage movies={movies} />
+  );
+};
+
+const rootElement = createRoot( document.getElementById("root") )
+rootElement.render(<App /> );
+
+const images = [
+    "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+    "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
+    "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
+    "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
+  ]
