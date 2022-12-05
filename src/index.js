8308cd8e8c4343f2b8e19d1ff61371df0e1178b4
Killian Halpin <you@example.com>
2022-12-05 15:28:49 +0000

    Home Page components.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
 import HomePage from "./pages/homePage";
-import sample from './stpries/sampleData'
+import sample from './stories/sampleData'
 //import MovieDetailsPage from './pages/movieDetailsPage'
 
 const movies = [sample, sample, sample, sample, sample, sample, sample];
